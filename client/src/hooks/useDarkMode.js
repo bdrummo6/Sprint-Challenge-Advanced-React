76f7a65528f25d3c57380e2fe87dc61dc3c6aca2
bdrummo6 <bradley.drummonds@gmail.com>
2020-04-19 20:59:14 -0400

    Created useDarkMode hook

---
+++ client/src/hooks/useDarkMode.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = (key, initialValue) => {
+	const [ value, setValue ] = useLocalStorage(key, initialValue);
+
+	useEffect(() => {
+		if (value) {
+			document.querySelector('body').classList.add('dark-mode');
+		} else {
+			document.querySelector('body').classList.remove('dark-mode');
+		}
+	}, [value])
+
+	return [value, setValue];
+}
+
+export default useDarkMode;
