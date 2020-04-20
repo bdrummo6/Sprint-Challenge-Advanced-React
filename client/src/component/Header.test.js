3dc2c6b04a540b9979d0bf54a799dea79a0e0aaa
bdrummo6 <bradley.drummonds@gmail.com>
2020-04-19 22:02:26 -0400

    Update Header.test.js

--- client/src/component/Header.test.js
+++ client/src/component/Header.test.js
@@ -8,13 +8,3 @@ test('h1 is rendering', () => {
 	const container = render(<Header />)
 	container.getByText(/Soccer Players from Around the World/i);
 });
-
-test('test dark mode button functionality', () => {
-	const header = render(<Header />);
-	const darkModeBtn = header.getByTestId(/darkModeBtn/i);
-	const darkModeOn = header.getByTestId(/darkModeOn/i);
-
-	fireEvent.click(darkModeBtn);
-
-	header.expect(darkModeOn.textContent).toBe('Dark Mode Off');
-});
