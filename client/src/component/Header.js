4607c1df009c4f0b16d291294266f395c7011105
bdrummo6 <bradley.drummonds@gmail.com>
2020-04-19 21:01:57 -0400

    DarkMode functionality created
    Imported useDarkMode hook into the header and then created a button that would toggle dark mode on and off when clicked.

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import useDarkMode from '../hooks/useDarkMode';
+
 const Header = () => {
+	const [ darkMode, setDarkMode ] = useDarkMode(false);
+
+	const toggle = event => {
+		event.preventDefault();
+		setDarkMode(!darkMode);
+	}
 
 	return (
 		<header className='header'>
 			<h1>Soccer Players from Around the World</h1>
+			<div>
+				<button className='darkModeBtn' onClick={toggle}>Dark Mode</button>
+			</div>
 		</header>
 	)
 };
